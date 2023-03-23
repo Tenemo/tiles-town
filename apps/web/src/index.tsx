8da5b35f169a9ddff9caefdaea1d0714abb45182
Piotr Piech <piotr.piech@nearform.com>
2023-03-23 19:38:59 +0100

    added sentry.io

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -1,8 +1,16 @@
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/js/bootstrap.min';
 
 import Root from 'components/Root';
+
+Sentry.init({
+    dsn: 'https://3838500c91374fb1803784b9975af290@o502294.ingest.sentry.io/4504889213386752',
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+});
 
 const container = document.getElementById('root');
 
